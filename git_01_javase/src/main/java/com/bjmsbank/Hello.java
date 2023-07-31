4e950bee12a97586a4307388296b08e39240a62b
60577 <605772398@qq.com>
2023-07-31 19:22:59 +0800

    项目经理完成了对hello类的更改，完成了one类的增加

--- git_01_javase/src/main/java/com/bjmsbank/Hello.java
+++ git_01_javase/src/main/java/com/bjmsbank/Hello.java
@@ -7,5 +7,6 @@ package com.bjmsbank;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("这是项目经理开发应用程序的第一个类。。。。。");
+        System.out.println("项目经理完成了对hello类的更改。。。。。");
     }
 }
---
+++ git_02_javaweb/src/main/java/com/bjmsbank/controller/One.java
@@ -0,0 +1,11 @@
+package com.bjmsbank.controller;
+
+/**
+ * @author alphastart
+ * @create 2023-07-31 19:21
+ */
+public class One {
+    public static void main(String[] args) {
+        System.out.println("项目经理完成了类的增加。。。。。");
+    }
+}
