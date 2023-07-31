9638e28b26e0dacc60251b6d617a94a7f924aa63
60577 <605772398@qq.com>
2023-07-31 19:32:32 +0800

    程序员完成Two类的增加，完成了web的更新

---
+++ git_01_javase/src/main/java/com/bjmsbank/Two.java
@@ -0,0 +1,11 @@
+package com.bjmsbank;
+
+/**
+ * @author alphastart
+ * @creat 2023-07-31 19:29
+ */
+public class Two {
+    public static void main(String[] args) {
+        System.out.println("程序员完成了Two类的增加。。。。。");
+    }
+}
--- git_02_javaweb/src/main/java/com/bjmsbank/controller/UserController.java
+++ git_02_javaweb/src/main/java/com/bjmsbank/controller/UserController.java
@@ -8,5 +8,6 @@ public class UserController {
     public static void main(String[] args) {
         System.out.println("这是项目经理开发的web应用程序。。。。。。。。");
         System.out.println("我是修改的内容");
+        System.out.println("程序员更新完成。。。。。。");
     }
 }
