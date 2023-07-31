80ad063535328ce047020eab7c31a89379edcf73
60577 <605772398@qq.com>
2023-07-31 17:44:43 +0800

    增加了一个demo类，修改了一个UserController类中的内容

---
+++ git_01_javase/src/main/java/com/bjmsbank/demo.java
@@ -0,0 +1,11 @@
+package com.bjmsbank;
+
+/**
+ * @author alphastart
+ * @create 2023-07-31 17:41
+ */
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("这是javase新增的一个类");
+    }
+}
--- git_02_javaweb/src/main/java/com/bjmsbank/controller/UserController.java
+++ git_02_javaweb/src/main/java/com/bjmsbank/controller/UserController.java
@@ -7,5 +7,6 @@ package com.bjmsbank.controller;
 public class UserController {
     public static void main(String[] args) {
         System.out.println("这是项目经理开发的web应用程序。。。。。。。。");
+        System.out.println("我是修改的内容");
     }
 }
