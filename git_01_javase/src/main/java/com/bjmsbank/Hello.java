4c5ee52534094520d2153fc7aa31a10c02c45c49
60577 <605772398@qq.com>
2023-07-31 17:19:56 +0800

    提交整个项目

---
+++ git_01_javase/src/main/java/com/bjmsbank/Hello.java
@@ -0,0 +1,11 @@
+package com.bjmsbank;
+
+/**
+ * @author alphastart
+ * @create 2023-07-31 17:12
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("这是项目经理开发应用程序的第一个类。。。。。");
+    }
+}
---
+++ git_02_javaweb/src/main/java/com/bjmsbank/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.bjmsbank.controller;
+
+/**
+ * @author alphastart
+ * @create 2023-07-31 17:17
+ */
+public class UserController {
+    public static void main(String[] args) {
+        System.out.println("这是项目经理开发的web应用程序。。。。。。。。");
+    }
+}
